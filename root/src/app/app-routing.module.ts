435f321dcd7644c02f5e560c3cf192f243122eef
yarkinn <82332307+yarkinn@users.noreply.github.com>
2022-07-07 16:16:20 +0300

    no msg

--- root/src/app/app-routing.module.ts
+++ root/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FirstComponent} from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'first-component', component: FirstComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
